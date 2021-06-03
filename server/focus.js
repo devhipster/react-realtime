23e6e7af7fad69661f15ae381185aa34c9cdba73
HipsterSantos <santoscampos269@gmail.com>
2021-06-03 03:19:33 +0100

    made change in order to make it running

--- server/focus.js
+++ server/focus.js
@@ -1,11 +1,11 @@
-import express from 'express'; // eslint-disable-line
-import {Socket as io} from 'socket.io';
-import mongoose  from 'mongoose';
-
+const express  = require('express'); // eslint-disable-line
+const mongoose = require('mongoose'); // eslint-disable-line
 const app = express();
-const server  = () => import (http).then(obj => obj.createServer(app))
-.catch(console.error);
 
+const server  = require('http').createServer(app);
+const io = require('socket.io')(server)
+//dynamic import let objs = awit import('module');
+// const server  = () => import (http).then(obj => obj.createServer(app))
 const {
   APP_PORT = 5000,
   NODE_ENV = 'devlopment'
@@ -13,6 +13,10 @@ const {
 
 const IN_PROD = NODE_ENV !== 'production';
 
-app.listen(5000,()=>{
+
+app.get('/home',(req,res)=>{
+  res.send('home is working perfeclty')
+})
+server.listen(5000,()=>{
   console.log(' listening on port 5000')
 })
